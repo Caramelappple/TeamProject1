2fceebbf2b397e633eae0179fd27276de776e90e
hwani165 <seungyung.kang@gmail.com>
2026-04-16 20:03:34 +0900

    feat : HealthSystem

--- Assets/_Scripts/KSY/KSY_IRecoverableResource.cs
+++
@@ -1,6 +0,0 @@
-using System;
-public interface KSY_IRecoverableResource
-{
-    public event Action<KSY_RecoverEventArgs> OnRecover;
-    public void Recover(int recoverValue);
-}
--- Assets/_Scripts/KSY/KSY_RecoverEventArgs.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-public class KSY_RecoverEventArgs
-{
-    public GameObject giver;
-    public int recoverValue;
-    public int resourceValue;
-}
-
