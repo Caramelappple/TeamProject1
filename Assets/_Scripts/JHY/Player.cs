11f30a447708e4f6a8a32ff592b06fc857d2fc25
jangbari1117 <ssjanghy@gmail.com>
2026-05-13 22:52:51 +0900

    save

--- Assets/_Scripts/JHY/Player.cs
+++ Assets/_Scripts/JHY/Player.cs
@@ -6,9 +6,19 @@ public class Player : MonoBehaviour
     private Rigidbody2D rb;
     [SerializeField] private float speed = 7f;
     private Vector2 moveDir;
+    private float currentSlowMultiplier = 1f;
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+    }
+    public void SetSlow(float multiplier)
+    {
+        currentSlowMultiplier = multiplier;
+    }
+
+    public void ClearSlow()
+    {
+        currentSlowMultiplier = 1f;
     }
     private void OnMove(InputValue value)
     {
@@ -16,6 +26,6 @@ public class Player : MonoBehaviour
     }
     private void FixedUpdate()
     {
-        rb.linearVelocity = moveDir * speed;
+        rb.linearVelocity = moveDir * speed*currentSlowMultiplier;
     }
 }
