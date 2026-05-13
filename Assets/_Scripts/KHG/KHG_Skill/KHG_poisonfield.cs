29be8d913959bb5d2d9af2e7e125108afdc67d78
Caramelappple <papersion1210@gmail.com>
2026-05-13 21:48:25 +0900

    ave

--- Assets/_Scripts/KHG/KHG_Skill/KHG_poisonfield.cs
+++ Assets/_Scripts/KHG/KHG_Skill/KHG_poisonfield.cs
@@ -6,62 +6,36 @@ public class KHG_PoisonField : MonoBehaviour//,LSO_ISkill
 {
     [SerializeField] private GameObject poisonPrefab;
     [SerializeField] private float spawnDistance = 1.5f;
-    [SerializeField] private float coolTime = 3f;
-    private LSO_PlayerMovement playerMovement;
-    private bool canUseSkill = true;
-
-    private void Update()
-    {
-        if (Keyboard.current.rKey.wasPressedThisFrame && canUseSkill)
-        {
-
-            //Vector2 lookDirection = playerMovement
-
-            //Vector3 spawnPos = transform.position + new Vector3(lookDirection.x, lookDirection.y, 0) * spawnDistance;
-
-            //GameObject poison = Instantiate(poisonPrefab, spawnPos, Quaternion.identity);
-
-            //// 오브젝트에 방향으로 속도 부여 (Rigidbody2D가 있을 경우)
-            //Rigidbody2D rb = poison.GetComponent<Rigidbody2D>();
-            //if (rb != null)
-            //{
-            //    rb.linearVelocity = lookDirection.normalized * 5f; // 발사 속도
-            //}
-
-            //Destroy(poison, 5f);
-
-            //StartCoroutine(SkillCoolTime());
-        }
-    }
-
-    private IEnumerator SkillCoolTime()
-    {
-        canUseSkill = false;
-        yield return new WaitForSeconds(coolTime);
-        canUseSkill = true;
-    }
-
+    private float _coolTime = 3f;
+    private LSO_PlayerMovement _playerMovement;
+    private bool _canUse = true;
+    
     public void UseSkill(GameObject player)
     {
-        //Vector2 lookDirection = playerMovement.GetLastDir();
+        if (!_canUse) return;
+        
+        _playerMovement = player.GetComponent<LSO_PlayerMovement>();
+        
+        Vector2 lookDirection = _playerMovement.GetLastDir();
 
-        //Vector3 spawnPos = transform.position + new Vector3(lookDirection.x, lookDirection.y, 0) * spawnDistance;
+        Vector3 spawnPos = transform.position + new Vector3(lookDirection.x, lookDirection.y, 0) * spawnDistance;
 
-        //GameObject poison = Instantiate(poisonPrefab, spawnPos, Quaternion.identity);
+        GameObject poison = Instantiate(poisonPrefab, spawnPos, Quaternion.identity);
 
-        //// 오브젝트에 방향으로 속도 부여 (Rigidbody2D가 있을 경우)
-        //Rigidbody2D rb = poison.GetComponent<Rigidbody2D>();
-        //if (rb != null)
-        //{
-        //    rb.linearVelocity = lookDirection.normalized * 5f; // 발사 속도
-        //}
+        //오브젝트에 방향으로 속도 부여 (Rigidbody2D가 있을 경우)
+        Rigidbody2D rb = poison.GetComponent<Rigidbody2D>();
+        if (rb != null)
+        {
+            rb.linearVelocity = lookDirection.normalized * 5f; // 발사 속도
+        }
 
-        //Destroy(poison, 5f);
-
+        Destroy(poison, 5f);
+        player.GetComponent<MonoBehaviour>().StartCoroutine(CoolTime(_coolTime));
     }
-
-    public IEnumerator CoolTime(float time)
+    private IEnumerator CoolTime(float time)
     {
-        throw new System.NotImplementedException();
+        _canUse = false;
+        yield return new WaitForSeconds(time);
+        _canUse = true;
     }
 }
