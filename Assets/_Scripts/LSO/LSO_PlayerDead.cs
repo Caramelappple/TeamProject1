48892f4b4e631b6577d66f0de466ba612843d13b
mindzeros <mindzeros@icloud.com>
2026-05-20 23:52:16 +0900

    save

--- Assets/_Scripts/LSO/LSO_PlayerDead.cs
+++ Assets/_Scripts/LSO/LSO_PlayerDead.cs
@@ -8,7 +8,9 @@ public class LSO_PlayerDead : MonoBehaviour
     private Health _health;
     private SpriteRenderer _sprite;
     private Animator _animator;
-    
+
+    [SerializeField] private KDH_MainMenuGo _menuGo;
+
     private LSO_Editor _editor;
     
     private void Awake()
@@ -42,6 +44,8 @@ public class LSO_PlayerDead : MonoBehaviour
         SetSat(-100);
         SetTra(0);
         Up();
+
+        _menuGo.MakeUI();
     }
     private void SetSat(float targetValue)
     {
