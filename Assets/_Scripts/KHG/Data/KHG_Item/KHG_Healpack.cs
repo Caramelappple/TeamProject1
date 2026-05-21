c1c1c756e40c46dd7b7a00d0ab48da7d0dc0f3a1
khg1028 <khg101028@goedu.kr>
2026-05-21 23:49:56 +0900

    save

--- Assets/_Scripts/KHG/Data/KHG_Item/KHG_Healpack.cs
+++ Assets/_Scripts/KHG/Data/KHG_Item/KHG_Healpack.cs
@@ -4,10 +4,16 @@ using KHG.Player;
 using UnityEngine;
 using KHG.ItemSystem;
 
-public class Healpack : MonoBehaviour, KHG_ICollectable
+public class KHG_Healpack : MonoBehaviour, KHG_ICollectable
 {
     [SerializeField] private int healAmount = 5;
     public event Action<KHG_ICollectable> OnCollected;
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        KHG_Player player = other.GetComponent<KHG_Player>();
+        if (player != null) 
+            Collect(player);
+    }
 
     public void Collect(KHG_Player collector)
     {
@@ -16,8 +22,10 @@ public class Healpack : MonoBehaviour, KHG_ICollectable
 
         if (hasHealth)
         {
-           RecoverData data =  new RecoverData(null, healAmount);
+            RecoverData data = new RecoverData(null, healAmount);
             health.Recover(data);
+            OnCollected?.Invoke(this);
+            Destroy(gameObject);
         }
     }
 }
