fbf639e8e8cbfb846808b7bf7b1806cb118ba278
Caramelappple <papersion1210@gmail.com>
2026-05-22 21:32:26 +0900

    soundback

--- Assets/_Scripts/KHG/KHG_Skill/Poison/KHG_poisonfield.cs
+++ Assets/_Scripts/KHG/KHG_Skill/Poison/KHG_poisonfield.cs
@@ -3,8 +3,6 @@ using UnityEngine;
 
 public class KHG_PoisonField : MonoBehaviour, LSO_ISkill
 {
-    [SerializeField] private AudioClip clip;
-    
     [SerializeField] private GameObject poisonPrefab;
 
     [SerializeField] private float spawnDistance = 1.5f;
@@ -15,8 +13,7 @@ public class KHG_PoisonField : MonoBehaviour, LSO_ISkill
     public void UseSkill(GameObject player)
     {
         if (!_canUse) return;
-        
-        LSO_SoundManager.Instance.SfxPlay(clip);
+
         LSO_PlayerMovement movement = player.GetComponent<LSO_PlayerMovement>();
         
 
@@ -24,8 +21,7 @@ public class KHG_PoisonField : MonoBehaviour, LSO_ISkill
 
         Vector2 dir = movement.GetFixedLastDir();
         Vector3 spawnPos = player.transform.position + (Vector3)(dir * spawnDistance);
-        
-        
+
         GameObject poison = Instantiate(poisonPrefab, spawnPos, Quaternion.identity);
         poison.GetComponent<KHG_PoisonDamage>().Init(player.GetComponent<Health>());
 
