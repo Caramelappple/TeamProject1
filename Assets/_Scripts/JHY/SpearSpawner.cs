26ec5adb9f3bce7e9334b736faa970970fc38a26
jangbari1117 <ssjanghy@gmail.com>
2026-05-23 09:55:29 +0900

    save

--- Assets/_Scripts/JHY/SpearSpawner.cs
+++ Assets/_Scripts/JHY/SpearSpawner.cs
@@ -5,11 +5,11 @@ public class SpearSpawner : MonoBehaviour
 {
     [SerializeField] private GameObject spearPrefab;
     [SerializeField] private GameObject warningCirclePrefab;
-    [SerializeField] private int spawnCount = 10;
+    [SerializeField] private int spawnCount = 20;
     [SerializeField] private float spawnDelay = 0.2f;
     [SerializeField] private float warningTime = 0.8f;
     [SerializeField] private float spawnHeightOffset = 3f;
-    [SerializeField] private Vector2 warningOffset = new Vector2(0f, -0.3f);
+    [SerializeField] private Vector2 warningOffset = new Vector2(0f, -0.8f);
     public void SpawnSpears()
     {
         StartCoroutine(SpawnSpearsRoutine());
