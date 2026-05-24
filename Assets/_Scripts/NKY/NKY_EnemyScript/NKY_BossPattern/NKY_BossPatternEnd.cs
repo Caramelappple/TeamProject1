94bfe8a1da125444bd1bd23fb1ffd08ac2b3ae07
Caramelappple <papersion1210@gmail.com>
2026-05-24 16:55:14 +0900

    minisave

--- Assets/_Scripts/NKY/NKY_EnemyScript/NKY_BossPattern/NKY_BossPatternEnd.cs
+++ Assets/_Scripts/NKY/NKY_EnemyScript/NKY_BossPattern/NKY_BossPatternEnd.cs
@@ -7,6 +7,7 @@ namespace _Scripts.NKY._EnemyScript.BossPattern
 {
     public class NKY_BossPatternEnd : MonoBehaviour
     {
+        [SerializeField] private float duration = 2f;
         private WaitForSeconds wait;
         private void Start()
         {
@@ -16,7 +17,7 @@ namespace _Scripts.NKY._EnemyScript.BossPattern
         public IEnumerator BossPatternEnd(Transform boss)
         {
             Vector3 endPos = new Vector3(Random.Range(-7f, 17f), Random.Range(-14f, 5f), 0);
-            boss.DOMove(endPos, 2f);
+            boss.DOMove(endPos, duration).SetEase(Ease.OutCubic);
             yield break;
         }
     }
