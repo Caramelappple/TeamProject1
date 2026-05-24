c6c44786f9032e2e56041220aea951a44a35e64b
nayona1st <nayona1st@gmail.com>
2026-05-24 18:14:34 +0900

    save

--- Assets/_Scripts/NKY/NKY_EnemyScript/BossPhaseEffects/NKY_BossPhase3Effect.cs
+++ Assets/_Scripts/NKY/NKY_EnemyScript/BossPhaseEffects/NKY_BossPhase3Effect.cs
@@ -21,10 +21,12 @@ namespace _Scripts.NKY._EnemyScript.BossPhaseEffects
         public override IEnumerator PlayPhaseEffect()
         {
             _bossAnimator.Play("Dead");
+            NKY_SoundManager.Instance.PlaySFX("Stun");
+            NKY_SoundManager.Instance.PlaySFX("ClashGlass");
             yield return WaitAnim(_bossAnimator, "Dead", 0.6f);
-            //임펙트링의 사이즈를 DoScale을 이용해 사이즈를 늘리고 투명화를 하여 천천히 사라지도록 하는 코루틴을 만들어서 예일드 리턴으로 반환
             ImpactRingEffect();
             _bossAnimator.Play("Resurrection");
+            NKY_SoundManager.Instance.PlaySFX("AttachGlass");
             yield return WaitUntilOrTime(() => false, 3f);
             _bossAnimator.Play("StandUp");
             yield return WaitAnim(_bossAnimator, "StandUp", 1f);
@@ -39,6 +41,7 @@ namespace _Scripts.NKY._EnemyScript.BossPhaseEffects
         private void ImpactRingEffect()
         {
             impectRing.SetActive(true);
+            NKY_SoundManager.Instance.PlaySFX("Impact");
             impectRing.transform.DOScale(new Vector2(ringSize, ringSize), ringDuration);
             _bossRingSr.DOFade(0, ringDuration);
             Camera.main.transform.DOShakePosition(ringDuration, 1.3f);
