38df21a22a2269aa16cf8e2955257715725af439
khg1028 <khg101028@goedu.kr>
2026-05-24 22:02:49 +0900

    save

--- Assets/_Scripts/KHG/Data/KHG_Item/KHG_Healpack.cs
+++ Assets/_Scripts/KHG/Data/KHG_Item/KHG_Healpack.cs
@@ -23,7 +23,8 @@ public class KHG_Healpack : MonoBehaviour, KHG_ICollectable
 
             OnCollected?.Invoke(this);
             
-            Destroy(gameObject);
+            
+            
         }
         else
         {
